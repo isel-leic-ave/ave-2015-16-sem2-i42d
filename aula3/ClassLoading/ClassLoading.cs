99b56a97dc4b3ce517e33dc8ce415cc3f406192f
jsimaoisel <jsimao@cc.isel.ipl.pt>
2016-03-08 15:12:54 +0000

    Aula 3: Código e demos

---
+++ aula3/ClassLoading/ClassLoading.cs
@@ -0,0 +1,50 @@
+public class B
+{
+    static B()
+    {
+        System.Console.WriteLine("@B.cctor");
+    }
+}
+
+public class C
+{
+    public int field = 10;
+    static C()
+    {
+        System.Console.WriteLine("@C.cctor");
+    }
+}
+
+public class D
+{
+    static D()
+    {
+        System.Console.WriteLine("@D.cctor");
+    }
+}
+
+public class A
+{
+    static A()
+    {
+        System.Console.WriteLine("@A.cctor");
+    }
+
+    public static void M(C c, D d)
+    {
+        System.Console.WriteLine("@A.M");
+        if (c!=null && c.field == 10)
+        {
+            System.Console.WriteLine("@A.M c != null");
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("@A.Main");
+        B b; // = new B();
+        System.Console.ReadLine();
+        A.M(null, null);
+        System.Console.ReadLine();
+    }
+}
---
+++ aula3/ILIntro/Sum.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace AVE
+{
+    public class Sum
+    {
+        public static int SumTwo(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static void Main()
+        {
+            //System.Console.WriteLine(SumTwo(2, 3));
+            Console.WriteLine(SumTwo(2, 3));
+        }
+    }
+}
