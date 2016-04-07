6dc890cd6d2d57c08479ad76c82418e3fa553be5
jsimaoisel <jsimao@cc.isel.ipl.pt>
2016-04-07 23:15:01 +0100

    aula 10: Exemplo com carregamento dinâmico de código - plugins para processamento de array de inteiros

---
+++ aula10/Aula10FilterPlugin/FiltersLibrary/FiltersLibrary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiltersLibrary
+{
+    public class Filters
+    {
+        private LinkedList<IFilter> filters = new LinkedList<IFilter>();
+
+        public void Add(IFilter f)
+        {
+            filters.AddLast(f);
+        }
+
+        public void Apply(int[] image)
+        {
+            foreach(IFilter f in filters)
+            {
+                f.Apply(image);
+            }
+        }
+
+    }
+}
---
+++ aula10/Aula10FilterPlugin/MyFilters/Simplefilters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFilters
+{
+    public class TwoFilters
+    {
+        public static void F1(int[] a)
+        {
+            Console.WriteLine("At TwoFilters.F1");
+        }
+        public static void F2(int[] a)
+        {
+            Console.WriteLine("At TwoFilters.F2");
+        }
+    }
+
+
+    public class FilterAdd1 : IFilter
+    {
+        public void Apply(int[] image)
+        {
+            for (int i = 0; i < image.Length; ++i)
+            {
+                image[i] += 1;
+            }
+        }
+    }
+
+    public class FilterMult2 : IFilter
+    {
+        public void Apply(int[] image)
+        {
+            for (int i = 0; i < image.Length; ++i)
+            {
+                image[i] *= 2;
+            }
+        }
+    }
+}
