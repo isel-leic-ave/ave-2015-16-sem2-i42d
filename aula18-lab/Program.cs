7b38aa5da2f784d9a2b47761622377a93ed021fe
jsimaoisel <jsimao@cc.isel.ipl.pt>
2016-05-04 23:00:18 +0100

    Aula 18: código para usar no guião do laboratório

---
+++ aula18-lab/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace yield
+{
+    class Program
+    {
+        static IEnumerable<int> Count123()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+        }
+
+        static IEnumerable<int> Count1ToN(int n)
+        {
+            for (int i = 1; i < n; ++i)
+            {
+                yield return i;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            foreach(int n in Count123())
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
