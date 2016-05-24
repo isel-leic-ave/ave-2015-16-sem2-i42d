fd6cb86c6b89881dc06f1c7c25939ade7662fbb4
jsimaoisel <jsimao@cc.isel.ipl.pt>
2016-05-24 16:27:35 +0100

    Aula 20 e 21: Exercício sobre genéricos e delegates

---
+++ aula21/Aula21Demos/Utils/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Utils
+{
+    static class DoubleArraysUtils
+    {
+        public static double[] sqrt(double[] a)
+        {
+            return a.Select(n => Math.Sqrt(n)).ToArray();
+        }
+
+        public static double[] duplicate(double[] a) {
+            return a.Select(n => n * 2).ToArray();
+        }
+
+        public static double[] square(double[] a) {
+            return a.Select(n => n * n).ToArray();
+        }
+    }
+}
